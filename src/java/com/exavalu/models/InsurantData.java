f18220026664697d4f1a8dc5f83a73df5ac6510d
ssinhasneha <124487617+ssinhasneha@users.noreply.github.com>
2023-02-23 15:03:50 +0530

    insurantDataModel (#12)

---
+++ src/java/com/exavalu/models/InsurantData.java
@@ -0,0 +1,267 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.exavalu.models;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Map;
+import org.apache.log4j.Logger;
+import org.apache.struts2.dispatcher.ApplicationMap;
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.ApplicationAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+/**
+ *
+ * @author sinha
+ */
+public class InsurantData extends ActionSupport implements ApplicationAware, SessionAware, Serializable  {
+    
+    
+    private int insuranceDataId;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String streetAddress;
+    private String country;
+    private String ZipCode;
+    private String occupation;
+    private String hobbies; 
+    private String website;
+    private String picture;
+    
+     
+   
+    private SessionMap<String, Object> sessionMap = (SessionMap) ActionContext.getContext().getSession();
+
+    private ApplicationMap map = (ApplicationMap) ActionContext.getContext().getApplication();
+
+    @Override
+    public void setApplication(Map<String, Object> application) {
+        setMap((ApplicationMap) application);
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        setSessionMap((SessionMap<String, Object>) (SessionMap) session);
+    }
+
+    
+    public String addInsurantData() throws Exception {
+        String result = "FAILURE";
+
+        boolean success = InsurantDataService.getInstance().doInsurantDataEntry(this);
+
+        if (success) {
+            System.out.println("Successfully Added Insurant Data");
+            result = "SUCCESS";
+        } else {
+            System.out.println("OOps your Insurant Data is not added");
+        }
+ 
+        return result;
+    }
+    /**
+     * @return the sessionMap
+     */
+    public SessionMap<String, Object> getSessionMap() {
+        return sessionMap;
+    }
+
+    /**
+     * @param sessionMap the sessionMap to set
+     */
+    public void setSessionMap(SessionMap<String, Object> sessionMap) {
+        this.sessionMap = sessionMap;
+    }
+
+    /**
+     * @return the map
+     */
+    public ApplicationMap getMap() {
+        return map;
+    }
+
+    /**
+     * @param map the map to set
+     */
+    public void setMap(ApplicationMap map) {
+        this.map = map;
+    }
+
+    /**
+     * @return the insuranceDataId
+     */
+    public int getInsuranceDataId() {
+        return insuranceDataId;
+    }
+
+    /**
+     * @param insuranceDataId the insuranceDataId to set
+     */
+    public void setInsuranceDataId(int insuranceDataId) {
+        this.insuranceDataId = insuranceDataId;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the dateOfBirth
+     */
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * @param dateOfBirth the dateOfBirth to set
+     */
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the streetAddress
+     */
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    /**
+     * @param streetAddress the streetAddress to set
+     */
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the ZipCode
+     */
+    public String getZipCode() {
+        return ZipCode;
+    }
+
+    /**
+     * @param ZipCode the ZipCode to set
+     */
+    public void setZipCode(String ZipCode) {
+        this.ZipCode = ZipCode;
+    }
+
+    /**
+     * @return the occupation
+     */
+    public String getOccupation() {
+        return occupation;
+    }
+
+    /**
+     * @param occupation the occupation to set
+     */
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    /**
+     * @return the hobbies
+     */
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    /**
+     * @param hobbies the hobbies to set
+     */
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    /**
+     * @return the website
+     */
+    public String getWebsite() {
+        return website;
+    }
+
+    /**
+     * @param website the website to set
+     */
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    /**
+     * @return the picture
+     */
+    public String getPicture() {
+        return picture;
+    }
+
+    /**
+     * @param picture the picture to set
+     */
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+    
+    
+    
+}
