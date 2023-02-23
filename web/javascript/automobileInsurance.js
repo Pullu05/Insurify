e83d621ad1d1e15f1b99cb1ffad9de7f912319a6
Anand Kumar <56770818+AnandBIT@users.noreply.github.com>
2023-02-23 10:39:12 +0530

    added subscription plan form, fixed js for changing the section and f… (#7)
    * added subscription plan form, fixed js for changing the section and fixed an unwanted change in index.jsp
    
    * added button color change on click
    
    * optimised js for handling section changes on button click

--- web/javascript/automobileInsurance.js
+++ web/javascript/automobileInsurance.js
@@ -1,84 +1,23 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-function showVehicleData() {
-    document.getElementById("sendData").style.display = "none";
-    document.getElementById("insurantData").style.display = "none";
-    document.getElementById("planData").style.display = "none";
-    document.getElementById("productData").style.display = "none";
-    document.getElementById("vehicleData").style.display = "block";
+function handleAutomobileSectionChange(event) {
+    const selectedBtn = event.currentTarget;
+    const targetSection = document.querySelector("#" + selectedBtn.dataset.sectionid);
+    const currentSectionInView = document.querySelector('.automobileFormSection.d-block');
+    const currentActiveBtn = document.querySelector('.custom-btn.btn-success');
 
-    document.getElementById("vehicle_btn").classList.add("btn-success");
+    // Removing the current section in view
+    currentSectionInView.classList.remove('d-block');
+    currentSectionInView.classList.add('d-none');
 
+    // Showing the target section in view
+    targetSection.classList.add('d-block');
+    targetSection.classList.remove('d-none');
 
-    document.getElementById("insurant_btn").classList.remove("btn-success");
-    document.getElementById("plan_btn").classList.remove("btn-success");
-    document.getElementById("quote_btn").classList.remove("btn-success");
-    document.getElementById("product_btn").classList.remove("btn-success");
+    // Same concept for buttons is used!
+    currentActiveBtn.classList.remove('btn-success');
+    currentActiveBtn.classList.add('btn-secondary');
+
+    selectedBtn.classList.add('btn-success');
+    selectedBtn.classList.remove('btn-secondary');
+
 
 }
-function showInsurantData() {
-    document.getElementById("vehicleData").style.display = "none";
-    document.getElementById("productData").style.display = "none";
-    document.getElementById("planData").style.display = "none";
-    document.getElementById("sendData").style.display = "none";
-    document.getElementById("insurantData").style.display = "block";
-
-    document.getElementById("insurant_btn").classList.add("btn-success");
-
-    document.getElementById("vehicle_btn").classList.remove("btn-success");
-    document.getElementById("vehicle_btn").classList.add("btn-secondary");
-    document.getElementById("product_btn").classList.remove("btn-success");
-    document.getElementById("plan_btn").classList.remove("btn-success");
-    document.getElementById("quote_btn").classList.remove("btn-success");
-}
-function showProductData() {
-    document.getElementById("vehicleData").style.display = "none";
-    document.getElementById("insurantData").style.display = "none";
-    document.getElementById("planData").style.display = "none";
-    document.getElementById("sendData").style.display = "none";
-    document.getElementById("productData").style.display = "block";
-
-    document.getElementById("product_btn").classList.add("btn-success");
-
-    document.getElementById("vehicle_btn").classList.remove("btn-success");
-    document.getElementById("vehicle_btn").classList.add("btn-secondary");
-    document.getElementById("insurant_btn").classList.remove("btn-success");
-    document.getElementById("plan_btn").classList.remove("btn-success");
-    document.getElementById("quote_btn").classList.remove("btn-success");
-}
-
-function showPlanData() {
-    document.getElementById("vehicleData").style.display = "none";
-    document.getElementById("insurantData").style.display = "none";
-    document.getElementById("sendData").style.display = "none";
-    document.getElementById("productData").style.display = "none";
-    document.getElementById("planData").style.display = "block";
-
-    document.getElementById("plan_btn").classList.add("btn-success");
-
-    document.getElementById("vehicle_btn").classList.remove("btn-success");
-    document.getElementById("vehicle_btn").classList.add("btn-secondary");
-    document.getElementById("insurant_btn").classList.remove("btn-success");
-    document.getElementById("quote_btn").classList.remove("btn-success");
-    document.getElementById("product_btn").classList.remove("btn-success");
-}
-
-function showQuoteData() {
-    document.getElementById("vehicleData").style.display = "none";
-    document.getElementById("insurantData").style.display = "none";
-    document.getElementById("planData").style.display = "none";
-    document.getElementById("productData").style.display = "none";
-    document.getElementById("sendData").style.display = "block";
-
-    document.getElementById("quote_btn").classList.add("btn-success");
-
-    document.getElementById("vehicle_btn").classList.remove("btn-success");
-    document.getElementById("vehicle_btn").classList.add("btn-secondary");
-    document.getElementById("insurant_btn").classList.remove("btn-success");
-    document.getElementById("plan_btn").classList.remove("btn-success");
-    document.getElementById("product_btn").classList.remove("btn-success");
-}
-
-
---
+++ web/subscriptionPlans.jsp
@@ -0,0 +1,52 @@
+<section class="container text-center rounded border border-dark p-4 mt-2 mx-auto">
+    <div class="row mx-0 gap-2 my-2">
+        <div class="col fw-bold text-white rounded-top p-2"></div>
+        <div class="col fw-bold text-white rounded-top p-2" style="background-color: #8e8e8e;">Silver</div>
+        <div class="col fw-bold text-white rounded-top p-2" style="background-color: #DAA520;">Gold</div>
+        <div class="col fw-bold text-white rounded-top p-2" style="background-color: #A0BFB4;">Platinum</div>
+        <div class="col fw-bold text-white rounded-top p-2" style="background-color: #1D63AF;">Ultimate</div>
+    </div>
+    <div class="row mx-0 gap-2 my-2">
+        <div class="col p-2 text-start">Price per Year ($)</div>
+        <div class="col fw-bold p-2">291.00</div>
+        <div class="col fw-bold p-2">857.00</div>
+        <div class="col fw-bold p-2">1,682.00</div>
+        <div class="col fw-bold p-2">3,204.00</div>
+    </div>
+    <div class="row mx-0 gap-2 my-2">
+        <div class="col bg-custom-gray p-2 text-start">Online Claim</div>
+        <div class="col bg-custom-gray fw-light p-2">No</div>
+        <div class="col bg-custom-gray fw-light p-2">Submit</div>
+        <div class="col bg-custom-gray fw-light p-2">Submit</div>
+        <div class="col bg-custom-gray fw-light p-2">Submit</div>
+    </div>
+    <div class="row mx-0 gap-2 my-2">
+        <div class="col p-2 text-start">Claims Discount (%)</div>
+        <div class="col p-2">No</div>
+        <div class="col p-2">2</div>
+        <div class="col p-2">5</div>
+        <div class="col p-2">10</div>
+    </div>
+    <div class="row mx-0 gap-2 my-2">
+        <div class="col bg-custom-gray p-2 text-start">Worldwide Cover</div>
+        <div class="col bg-custom-gray fw-light p-2">No</div>
+        <div class="col bg-custom-gray fw-light p-2">Limited</div>
+        <div class="col bg-custom-gray fw-light p-2">Limited</div>
+        <div class="col bg-custom-gray fw-light p-2">Unlimited</div>
+    </div>
+    <div class="row mx-0 gap-2 my-2">
+        <div class="col p-2 text-start">Select Option</div>
+        <div class="col p-2">
+            <input type="radio" class="form-check-input custom-border" name="radioNoLabel" />
+        </div>
+        <div class="col p-2">
+            <input type="radio" class="form-check-input custom-border" name="radioNoLabel" />
+        </div>
+        <div class="col p-2">
+            <input type="radio" class="form-check-input custom-border" name="radioNoLabel" />
+        </div>
+        <div class="col p-2">
+            <input type="radio" class="form-check-input custom-border" name="radioNoLabel" />
+        </div>
+    </div>
+</section>
