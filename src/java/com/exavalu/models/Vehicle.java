8d4e5eb3d967f4fdb238e7a2bfb6a776185ffbb6
Subhadip Sarkar <edusubhadip@gmail.com>
2023-02-23 13:46:30 +0530

    added vehicle model

---
+++ src/java/com/exavalu/models/Vehicle.java
@@ -0,0 +1,130 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.exavalu.models;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.util.logging.Logger;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
+import org.apache.struts2.dispatcher.ApplicationMap;
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.ApplicationAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+/**
+ *
+ * @author Subhadip Sarkar
+ */
+public class Vehicle extends ActionSupport implements ApplicationAware, SessionAware, Serializable {
+
+    private int enginePerformance, numberOfSeats, listPrice, annualMileage;
+    private String licensePlateNumber, make, fuelType;
+    private Date dateOfManufacture;
+
+    public int getEnginePerformance() {
+        return enginePerformance;
+    }
+
+    public void setEnginePerformance(int enginePerformance) {
+        this.enginePerformance = enginePerformance;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(int listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public int getAnnualMileage() {
+        return annualMileage;
+    }
+
+    public void setAnnualMileage(int annualMileage) {
+        this.annualMileage = annualMileage;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Date getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+
+    public void setDateOfManufacture(Date dateOfManufacture) {
+        this.dateOfManufacture = dateOfManufacture;
+    }
+
+    public SessionMap<String, Object> getSessionMap() {
+        return sessionMap;
+    }
+
+    public void setSessionMap(SessionMap<String, Object> sessionMap) {
+        this.sessionMap = sessionMap;
+    }
+
+    public ApplicationMap getMap() {
+        return map;
+    }
+
+    public void setMap(ApplicationMap map) {
+        this.map = map;
+    }
+
+    public static Logger getLOG() {
+        return LOG;
+    }
+
+    public static void setLOG(Logger LOG) {
+        ActionSupport.LOG = LOG;
+    }
+
+    private SessionMap<String, Object> sessionMap = (SessionMap) ActionContext.getContext().getSession();
+
+    private ApplicationMap map = (ApplicationMap) ActionContext.getContext().getApplication();
+
+    @Override
+    public void setApplication(Map<String, Object> application) {
+        map = (ApplicationMap) application;
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        sessionMap = (SessionMap) session;
+    }
+
+}
